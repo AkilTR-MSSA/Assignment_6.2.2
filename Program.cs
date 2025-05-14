306cf4c14567d34ffe8c578b1119c687c3032fd2
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-14 08:08:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int[] nums = { 1, 2, 3, 4 };
+Console.WriteLine(string.Join(", ", ProductExceptSelf(nums)));
+
+int[] ProductExceptSelf(int[] nums)
+{
+    int[] answer = new int[nums.Length];
+    int prefix = 1;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+    int postfix = 1;
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        answer[i] *= postfix;
+        postfix *= nums[i];
+    }
+    return answer;
+}
